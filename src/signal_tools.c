9ea9b86b15ed73f199befd8683dec601daae386f
Yu Chen <ychen2@z1t3c3.42bangkok.com>
2023-12-31 14:13:55 +0700

    fix uninitialized problem and sigquit

--- src/signal_tools.c
+++ src/signal_tools.c
@@ -6,19 +6,11 @@
 /*   By: ychen2 <ychen2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 18:12:46 by ychen2            #+#    #+#             */
-/*   Updated: 2023/12/31 14:01:23 by ychen2           ###   ########.fr       */
+/*   Updated: 2023/12/31 14:13:48 by ychen2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-// void	do_nothing(int signum)
-// {
-// 	g_sig = signum;
-// 	rl_on_new_line();
-// 	rl_replace_line("", 0);
-// 	rl_redisplay();
-// }
 
 void	read_again(int signum)
 {
